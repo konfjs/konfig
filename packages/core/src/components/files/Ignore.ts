c4119a6c7bfb88c9a9a9868988622caed263a245
Shinebayar G. <3091558+shinebayar-g@users.noreply.github.com>
2024-12-17 09:17:20 -0800

    chore: move files around (#4)

---
+++ packages/core/src/components/files/Ignore.ts
@@ -0,0 +1,4 @@
+export class IgnoreFile {
+    readonly name: string;
+    constructor(name: string) {}
+}
---
+++ packages/core/src/projects/typescript.ts
@@ -0,0 +1,6 @@
+import { TsConfig } from '../components/TsConfig.js';
+
+export class TypescriptProject {
+    readonly tsConfig: TsConfig;
+    constructor() {}
+}
