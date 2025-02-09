a78745ad40aea2036cbb2451d87b0ca404f07859
Shinebayar Gansukh <3091558+shinebayar-g@users.noreply.github.com>
2025-02-09 14:34:07 -0800

    docs: set github url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://konfjs.github.io',
+    base: 'konfig',
     integrations: [
         starlight({
             title: 'My Docs',
